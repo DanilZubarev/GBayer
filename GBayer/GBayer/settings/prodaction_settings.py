037730b5aa66e501ff1c5a858821ac22d76503a4
ZubarevDanil@gmail.com <Danilkms1987>
2023-01-22 01:25:38 +0700

    AAAAA

--- GBayer/GBayer/settings/prodaction_settings.py
+++ GBayer/GBayer/settings/prodaction_settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -144,3 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'madia')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/'
+
+ADMINS = [
+    ('Danil', 'ZubarevDanil@gmail.com'),
+]
